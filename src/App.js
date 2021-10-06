25cfc96552346f0d0af5050aa51a956f8a48f33d
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-06 23:13:19 +0600

    Show user logged in data in the UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
 import initAuthentication from './Firebase/firebase.init';
 initAuthentication();
 
 const provider = new GoogleAuthProvider();
 
-
-
 function App() {
-  
-const handleGoogleSignIn = () => {
-  const auth = getAuth();
-  signInWithPopup(auth,provider)
-  .then((result) => {
-    const user = result.user;
-  }) 
-}
+  const [user,setUser] = useState({});
+  const handleGoogleSignIn = () => {  
+    const auth = getAuth();
+    signInWithPopup(auth,provider)
+    .then((result) => {
+      const userLogged = result.user;
+      console.log(userLogged);
+      const loggedInUser = {
+        name : userLogged.displayName,
+        email : userLogged.email,
+        photo : userLogged.photoURL
+      };
+      setUser(loggedInUser);
+    }) 
+  }
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <br />
+      {
+        user.email && <div>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
