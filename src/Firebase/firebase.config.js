36c9c338325b07217bf9e514d40a571c0c7cd146
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-06 21:58:11 +0600

    firebase setup

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCb1JVkY_hO-iFbVcbF2Z6Ic8uoSiGrIL0",
+    authDomain: "react-firebase-authentic-13e76.firebaseapp.com",
+    projectId: "react-firebase-authentic-13e76",
+    storageBucket: "react-firebase-authentic-13e76.appspot.com",
+    messagingSenderId: "282996689464",
+    appId: "1:282996689464:web:02d6a1618137ba5cf134cd"
+  };
+
+export default firebaseConfig;
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+
+const initAuthentication = () => {
+    const app = initializeApp(firebaseConfig);
+}
+
+export default initAuthentication;
